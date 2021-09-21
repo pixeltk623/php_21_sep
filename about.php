2512e123a21e3a5fb2c99fdcc8a9ac8822bc80a4
shreetuition <info@shreetuitioncenter.com>
2021-09-21 08:27:13 +0530

    Done

---
+++ about.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HTML - About</title>
+	<style type="text/css"></style>
+	<script type="text/javascript"></script>
+	<link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+<!-- 	This is Kumar
+	This is Kumar -->
+	<!-- This is Kumar
+	This is Kumar
+	This is Kumar 
+	
+	1. Block Level Elements (h, p, div)
+	2. Inline Level Elements (span, a, img. input)
+	-->
+
+	<!-- <span>Test</span>
+	<span>Test</span>
+	<span>Test</span>
+	<span>Test</span>
+	<span>Test</span>
+
+	<h1>This is HTML</h1>
+	<h2>This is HTML</h2>
+	<h3>This is HTML</h3>
+	<h4>This is HTML</h4>
+	<h5>This is HTML</h5>
+	<h6>This is HTML</h6> -->	
+
+<!-- 	<center><span>This is Span</span></center> -->
+
+	<center><h1>Chennai Super Kings</h1></center>
+
+	<a href="index.php">Home</a>
+	<a href="about.php">About</a>
+	<a href="service.php">Service</a>
+	<a href="blog.php">Blog</a>
+	<a href="contact.php">Contact</a>
+	<hr>
+
+	<p><b>The Chennai Super Kings (CSK) </b>is a franchise cricket team based in Chennai, Tamil Nadu. They play in the Indian Premier League (IPL). Founded in 2008, the team plays its home matches at the M. A. Chidambaram Stadium in Chennai. The team is owned by Chennai Super Kings Cricket Ltd and India Cements is the major stakeholder. The team served a two-year suspension from the IPL starting July 2015 due to the involvement of their owners in the 2013 IPL betting case,[2] and won the title in its comeback season of 2018. The team is captained by <strong>Mahendra Singh Dhoni</strong> and coached by Stephen Fleming.</p>
+
+	<p>The Super Kings have won the IPL title thrice (in 2010, 2011 and 2018), and have the highest win percentage of matches among all teams in the IPL (59.83%).[3] They hold the records of most appearances in the playoffs (ten) and the Final (eight) of the IPL.[4] In addition, they have also won the Champions League Twenty20 in 2010 and 2014. The brand value of the Super Kings in 2019 is estimated to be around ₹732 crore (roughly $104 million), making them one of the most valuable IPL franchise. </p>
+	<h2>Franchise history</h2>
+	<hr>
+	<p>In September 2007, the Board of Control for Cricket in India (BCCI) announced the establishment of the Indian Premier League, a Twenty20 competition to be started in 2008.[5] In January 2008, the BCCI unveiled the owners of eight city-based franchises. The Chennai franchise was sold to the India Cements for $91 million, making it the fourth most expensive team in the league behind Mumbai, Bangalore and Hyderabad.[6] India Cements acquired the rights to the franchise for 10 years. Former ICC Chairman N. Srinivasan was the de facto owner of the Chennai Super Kings, by means of his position as the vice-chairman and managing director of India Cements Ltd. The franchisee was transferred to a separate entity named Chennai Super Kings Cricket Ltd., after the Supreme Court of India struck down the controversial amendment to the BCCI constitution's clause 6.2.4 that allowed board officials to have a commercial interest in the IPL and the Champions League Twenty20 on 22 January 2015.[7]</p>
+	<h2>Franchise history</h2>
+	<hr>
+	<p>In September 2007, the Board of Control for Cricket in India (BCCI) announced the establishment of the Indian Premier League, a Twenty20 competition to be started in 2008.[5] In January 2008, the BCCI unveiled the owners of eight city-based franchises. The Chennai franchise was sold to the India Cements for $91 million, making it the fourth most expensive team in the league behind Mumbai, Bangalore and Hyderabad.[6] India Cements acquired the rights to the franchise for 10 years. Former ICC Chairman N. Srinivasan was the de facto owner of the Chennai Super Kings, by means of his position as the vice-chairman and managing director of India Cements Ltd. The franchisee was transferred to a separate entity named Chennai Super Kings Cricket Ltd., after the Supreme Court of India struck down the controversial amendment to the BCCI constitution's clause 6.2.4 that allowed board officials to have a commercial interest in the IPL and the Champions League Twenty20 on 22 January 2015.[7]</p>
+	<h2>Franchise history</h2>
+	<hr>
+	<p>In September 2007, the Board of Control for Cricket in India (BCCI) announced the establishment of the Indian Premier League, a Twenty20 competition to be started in 2008.[5] In January 2008, the BCCI unveiled the owners of eight city-based franchises. The Chennai franchise was sold to the India Cements for $91 million, making it the fourth most expensive team in the league behind Mumbai, Bangalore and Hyderabad.[6] India Cements acquired the rights to the franchise for 10 years. Former ICC Chairman N. Srinivasan was the de facto owner of the Chennai Super Kings, by means of his position as the vice-chairman and managing director of India Cements Ltd. The franchisee was transferred to a separate entity named Chennai Super Kings Cricket Ltd., after the Supreme Court of India struck down the controversial amendment to the BCCI constitution's clause 6.2.4 that allowed board officials to have a commercial interest in the IPL and the Champions League Twenty20 on 22 January 2015.[7]</p>
+	<h2>Franchise history</h2>
+	<hr>
+	<p>In September 2007, the Board of Control for Cricket in India (BCCI) announced the establishment of the Indian Premier League, a Twenty20 competition to be started in 2008.[5] In January 2008, the BCCI unveiled the owners of eight city-based franchises. The Chennai franchise was sold to the India Cements for $91 million, making it the fourth most expensive team in the league behind Mumbai, Bangalore and Hyderabad.[6] India Cements acquired the rights to the franchise for 10 years. Former ICC Chairman N. Srinivasan was the de facto owner of the Chennai Super Kings, by means of his position as the vice-chairman and managing director of India Cements Ltd. The franchisee was transferred to a separate entity named Chennai Super Kings Cricket Ltd., after the Supreme Court of India struck down the controversial amendment to the BCCI constitution's clause 6.2.4 that allowed board officials to have a commercial interest in the IPL and the Champions League Twenty20 on 22 January 2015.[7]</p>
+	<h2>Franchise history</h2>
+	<hr>
+	<p>In September 2007, the Board of Control for Cricket in India (BCCI) announced the establishment of the Indian Premier League, a Twenty20 competition to be started in 2008.[5] In January 2008, the BCCI unveiled the owners of eight city-based franchises. The Chennai franchise was sold to the India Cements for $91 million, making it the fourth most expensive team in the league behind Mumbai, Bangalore and Hyderabad.[6] India Cements acquired the rights to the franchise for 10 years. Former ICC Chairman N. Srinivasan was the de facto owner of the Chennai Super Kings, by means of his position as the vice-chairman and managing director of India Cements Ltd. The franchisee was transferred to a separate entity named Chennai Super Kings Cricket Ltd., after the Supreme Court of India struck down the controversial amendment to the BCCI constitution's clause 6.2.4 that allowed board officials to have a commercial interest in the IPL and the Champions League Twenty20 on 22 January 2015.[7]</p>
+
+	<hr>
+
+	
+
+<!-- 	<center><span>This is Span</span>
+		<h1>Chennai Super Kings</h1>
+		<h1>Chennai Super Kings</h1> </center>
+		<h1>Chennai Super Kings</h1><h1>Chennai Super Kings</h1>
+		<h1>Chennai Super Kings</h1> -->
+
+</body>
+</html>
