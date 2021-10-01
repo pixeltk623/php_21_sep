6e44f03382e5dc3f4d7bbbc58716a84b4adf2af9
russia99 <88884183+russia99@users.noreply.github.com>
2021-10-01 11:52:37 +0530

    Update 27sep3.php

--- amit/27sephomework/27sep3.php
+++ amit/27sephomework/27sep3.php
@@ -6,48 +6,52 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>27sep3</title>
+    <style>
+        
+    </style>
 </head>
 
 <body>
     <h4>Registration form</h4>
-    <table border="1" width="100%" style="background-color: #e8e8e8;">
-        <form action="" method="POST">
 
+        <table border="1" width="100%" style="background-color: #e8e8e8; border: 2px solid black;">
+            <form action="" method="POST">
+                
             <tr>
                 <td>Enter your first name</td>
                 <td><input type="text" placeholder="enter your first name"></td>
             </tr>
-
+            
             <tr>
                 <td>Enter your last name</td>
                 <td><input type="text"></td>
             </tr>
-
+            
             <tr>
                 <td>Enter your password</td>
                 <td><input type="password"></td>
             </tr>
-
+            
             <tr>
                 <td>ReEnter your password</td>
                 <td><input type="password"></td>
             </tr>
-
+            
             <tr>
                 <td>Enter your email</td>
                 <td><input type="email" name="email"></td>
             </tr>
-
+            
             <tr>
                 <td>Enter your mobile</td>
                 <td><input type="tel" name="tel"></td>
             </tr>
-
+            
             <tr>
                 <td>Enter your address</td>
                 <td><textarea name="" id="" cols="16" rows="7"></textarea></td>
             </tr>
-
+            
             <tr>
                 <td>Select your gender</td>
                 <td>
@@ -55,7 +59,7 @@
                     <input type="radio" name="gender" value="Female">Female
                 </td>
             </tr>
-
+            
             <tr>
                 <td>Select your hobbies</td>
                 <td>
@@ -70,7 +74,7 @@
                     <input type="date" name="" value="dd-mm-yyyy">
                 </td>
             </tr>
-
+            
             <tr>
                 <td>Select your Country</td>
                 <td>
@@ -80,16 +84,16 @@
                     </select>
                 </td>
             </tr>
-
+            
             <tr>
                 <td>upload your pic</td>
                 <td> <input type="file" name="Profile">
-                </td>
-            </tr>
-
-            <tr>
-                <td colspan="2"> 
-                    <input type="submit" name="save my data">
+            </td>
+        </tr>
+        
+        <tr>
+            <td colspan="2"> 
+                <input type="submit" name="save my data">
                     <input type="reset" name="reset">
                 </td>
             </tr>
