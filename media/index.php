32a5b28b1db7cbb2e82221d0e611682633232c96
shreetuition <info@shreetuitioncenter.com>
2021-10-06 21:32:16 +0530

    Done

---
+++ media/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css"></style>
+</head>
+<body>
+	<h1>Media In HTML5 </h1>
+
+	<h1>Video</h1>
+
+	<button type="button" onclick="playAndPause()">Play/Pause</button>
+
+	<button type="button" onclick="makeBig()">Make Big</button>
+
+	<button type="button" onclick="makeNormal()">Make Normal</button>
+
+	<br>
+	<br>
+
+	<video id="myvideo" controls width="500" controlsList = "nodownload">
+		<source src="../assets/video1.mp4" type="video/mp4">
+	</video>
+
+
+	<!-- <h1>Video</h1>
+
+	<video controls width="500" controlsList = "nodownload" muted autoplay>
+		<source src="../assets/video1.mp4" type="video/mp4">
+	</video> -->
+
+	<!-- <h2>Audio</h2>
+
+	<audio controls controlsList = "nodownload" autoplay muted>
+		<source src="../assets/2.mp3" type="audio/mp3">
+	</audio> -->
+
+	<!-- <h2>YouTube</h2>
+
+	<iframe src="https://www.youtube.com/embed/6o5Vd0BcAbA?autoplay=0&&mute=1&list=PLd9tDukllEepYrfX-zy6NLbLRmHdPsukE" width="500" height="300" allowfullscreen ></iframe> -->
+
+	<script type="text/javascript">
+		var myvideo = document.getElementById('myvideo');
+
+		//alert(myvideo)
+
+		console.log(myvideo);
+		function playAndPause() {
+			
+			//console.log(myvideo.paused);
+
+			if (myvideo.paused) {
+				myvideo.play();
+			} else {
+				myvideo.pause();
+
+			}
+		}
+
+		function makeBig() {
+			myvideo.width = "800";
+		}
+
+		function makeNormal() {
+			myvideo.width = "500";
+		}
+	</script>
+</body>
+</html>
