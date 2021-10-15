60379a7e1190406819f03a09be682309093da638
shreetuition <info@shreetuitioncenter.com>
2021-10-15 09:32:05 +0530

    Done

---
+++ Js/control.js
@@ -0,0 +1,106 @@
+'use strict'
+
+// if (true) {}
+
+// if (true) {} else {}
+
+// if (true) {} else if {} else if (true) {} else {}
+
+// switch(){
+// 	case '':
+// 		break
+// }
+
+var x = 5
+
+var y = 8
+
+var z = 15
+//console.log(x<y);
+
+// if (x>y) {
+// 	console.log('Print');
+// }
+
+
+// if (x<y) {
+// 	console.log("Ok")
+// } else {
+// 	console.log('Else Block');
+// }
+
+
+// if (x>=y && x>=z) {
+// 	console.log('X is Greater Bumber', x);
+// } else if(y>=x && y>=z) {
+// 	console.log('Y is Greater Bumber', y);
+// } else {
+// 	console.log('Z is Greater Bumber', z);
+// }
+
+
+// var op = '*';
+// var result;
+// switch(op) {
+// 	case '+':
+// 		result = x + y
+// 		//console.log(result);
+// 		break;
+// 	case '-':
+// 		result = x - y
+// 		break;
+// 	case '*':
+// 		result = x * y
+// 		break;
+// 	case '/':
+// 		result = x / y
+// 		break;
+// 	case '%':
+// 		result = x % y
+// 		break;
+// 	default:
+// 		result = 'Error'
+// 		break;
+// }
+
+// console.log(result);
+
+
+function makeCalculator() {
+
+	var fnum = parseInt(document.getElementById("fn").value)
+	var snum = parseInt(document.getElementById("sn").value)
+	var op = document.getElementById("op").value
+	// console.log(fnum + snum + op);
+
+	// console.log(typeof(fnum))
+	var result
+
+	switch(op) {
+		case '+':
+			result = fnum + snum;
+			break;
+		case '-':
+			result = fnum - snum;
+			break;
+		case '*':
+			result = fnum * snum;
+			break;
+		case '/':
+			result = fnum / snum;
+			break;
+	}
+
+	console.log(result);
+
+	var html = "<table border='1' style='border-collapse: collapse; width: 500px;'>\
+		<tr>\
+			<th>Result</th>\
+			<td>"+result+"</td>\
+		</tr>\
+	</table>"
+
+	document.getElementById("res").innerHTML = html;
+
+	return false;
+}
---
+++ Js/custom.js
@@ -0,0 +1,45 @@
+// console.log('Hello');
+// alert()
+
+// UserDefined Function
+// Pre-defined inbuily function
+
+// function getMyName() {
+
+// 	console.log('My Name is Kumar');
+// } 
+
+// getMyName()
+
+function clickMe() {
+
+	console.log('Yes ! Clicked');
+}
+
+// document.getElementById
+// document.getElementsByName 
+// document.getElementsByClassName
+// document.getElementsByTagName
+
+// var tagName = document.getElementsByTagName("h1")
+
+// console.log(tagName);
+
+// var getName = document.getElementsByName("gender");
+
+// console.log(getName);
+
+// var h1Id = document.getElementById("h1");
+
+// var  h1 = document.getElementsByClassName("h1");
+
+
+//console.log(h1Id.style.color = "Red");
+// console.log(h1);
+// console.log(h1[0].outerHTML);
+// console.log(h1[0].innerHTML);
+// // cssText 
+// console.log(h1[0].style);
+// console.log(h1[0].style.backgroundColor = "red");
+
+// console.log(h1[0].style.cssText = 'color:red; font-size:50px; background-color: black; cursor: pointer;');
