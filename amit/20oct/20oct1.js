8a55a0d42159dd1cf7c4d574eb3f0c7b1e9df206
russia99 <88884183+russia99@users.noreply.github.com>
2021-10-25 20:18:30 +0530

    done

--- amit/20oct/20oct1.js
+++ amit/20oct/20oct1.js
@@ -37,41 +37,84 @@
 // (timer)
 
 
-var min    = 00;
-var second = 00;
-var hour = 00;
-var zeroPlaceholder = 0;
-var counterId = setInterval(function(){
-                  countUp();
-                }, 1000);
+// var min    = 0;
+// var second = 00;
+// var hour = 0;
+// var zeroofsecond = 0;
+// var zeroofmin = 0;
+// var counterId = setInterval(function(){
+//                   countUp();
+//                 }, 1000);
 
-function countUp () {
-    second++;
-    if(second == 60){
-        second = 00;
-        min = min + 1;
-    }
-    if (min == 60) {
-        min = 00;
-        hour = hour + 1;
-    }
+// function countUp () {
+//     second++;
+//     if(second == 60){
+//         second = 00;
+//         min = min + 1;
+//     }
+//     if (min == 60) {
+//         min = 00;
+//         hour = hour + 1;
+//     }
 
-    if (min == 10){
-        zeroPlaceholder = '';
-    } else if (min == 00) {
-        zeroPlaceholder = 0;
+//     if (min == 10){
+//         zeroofmin = '';
+//     } else if (min == 00) {
+//         zeroofmin = 0;
+//     }
+
+//     if(second == 10){
+//         zeroofsecond = '';
+//     }else
+//     if(second == 00){
+//         zeroofsecond = 0;
+//     }
+
+//     document.getElementById("count-up").innerText = hour+':'+zeroofmin+min+':'+zeroofsecond+second;
+// }
+
+// function showTime() {
+//     var date = new Date();
+//     var h = date.getHours();
+//     var m = date.getMinutes();
+//     var s = date.getSeconds();
+//     var session = "AM";
+
+//     if (h > 12) {
+//         h = h - 12;
+//         session = "PM";
+//     }
+
+
+//     if (h < 10) {
+//         h = "0" + h;
+//     }
+//     if (m < 10) {
+//         m = "0" + m;
+//     }
+//     if (s < 10) {
+//         s = "0" + s;
+//     }
+
+//     var time = h + ":" + m + ":" + s + " " + session;
+
+//     document.getElementById("count-up").innerHTML = time;
+//     setInterval(showTime, 1000);
+
+// }
+
+// showTime();
+
+// ------------------------------------------------------------------------------------------------------
+
+let a = [[1, 2], [3, 4]];
+let b = [[2, 3], [4, 5]];
+let c = [[], []];
+
+for (let i = 0; i < 2; i++) {
+    for (let j = 0; j < 2; j++) {
+        c[i][j] = a[i][j] + b[i][j];
     }
-    
-    if(second == 10){
-        zeroPlaceholder = '';
-    }else
-    if(second == 00){
-        zeroPlaceholder = 0;
-    }
-    
-    document.getElementById("count-up").innerText = hour+':'+zeroPlaceholder+min+':'+zeroPlaceholder+second;
 }
 
-
-
-
+console.log(c);
---
+++ amit/22oct/22oct.js
@@ -0,0 +1,78 @@
+
+function formCheckValidation() {
+
+    var name = document.getElementById("name").value;
+    var Email = document.getElementById("email").value;
+    var genders = document.getElementsByName("gender");
+    var hobby = document.getElementsByName("hobby[]");
+    var cities = document.getElementById("city");
+    var cityvalid = cities.value;
+    var file = document.getElementById("fileUpload");
+    var fileUpload = file.value;
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const filevalidation = /[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\%\+\~\_ ]+$/;
+    
+
+    if (name == "") {
+        document.getElementById("e1").innerHTML = "fill the name";
+    } else {
+        document.getElementById("e1").innerHTML = ""; 
+    }
+
+
+    if (Email == "") {
+        document.getElementById("e2").innerHTML = "email can't be blank";
+    } else {
+        if (re.test(Email) == true) {
+            document.getElementById("e2").innerHTML = "";
+        } else if (re.test(Email) == false) {
+            document.getElementById("e2").innerHTML = "wrong email";
+        }
+    }
+
+
+    if (genders[0].checked || genders[1].checked) {
+        document.getElementById("e3").innerHTML = "";
+    } else {
+        document.getElementById("e3").innerHTML = "select your gender";
+    }
+
+    var hoobyList = [];
+    for (var i = 0; i < hobby.length; i++) {
+
+        if (hobby[i].checked) {
+            hoobyList.push(hobby[i].value)
+        }
+    }
+
+
+    if (hoobyList.length == 0) {
+        document.getElementById("e4").innerHTML = "select your hobby";
+    } else {
+
+        if (hoobyList.length < 2) {
+            document.getElementById("e4").innerHTML = "atleast two hobby must be selected";
+        } else {
+            document.getElementById("e4").innerHTML = "";
+        }
+
+    }
+
+    if (cityvalid == "select") {
+        document.getElementById("e5").innerHTML = "select your city";
+    } else {
+        document.getElementById("e5").innerHTML = "";
+    }
+
+    if (fileUpload == "") {
+        document.getElementById("e6").innerHTML = "file should be uploaded";
+    } else {
+        if (filevalidation.test(fileUpload) == true) {
+            document.getElementById("e6").innerHTML = "";
+        } else if (filevalidation.test(fileUpload) == false) {
+            document.getElementById("e6").innerHTML = "wrong file format";
+        }
+    }
+
+    return false;
+}
