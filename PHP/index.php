9d79775c86a6de226715b240a80ee02ce0c89334
shreetuition <info@shreetuitioncenter.com>
2021-12-03 17:02:15 +0530

    Done

---
+++ PHP/index.php
@@ -0,0 +1,76 @@
+<?php 
+
+//$a = 5;
+
+//echo isset($a);
+
+// if (isset($a)) {
+	
+// 	echo $a;
+// }
+
+//echo $a;
+
+//die;
+// $a = false;
+
+// var_dump($a);
+// echo $a;
+
+$firstName = "Sharvan";
+$lastName = "Kumar";
+//echo $firstName." ".$lastName;
+//die()
+// 	PHP Variables
+// A variable can have a short name (like x and y) or a more descriptive name (age, carname, total_volume).
+
+// camcale case $carName
+// snake case $car_name
+// pascale case $CarName
+
+// Rules for PHP variables:
+
+// A variable starts with the $ sign, followed by the name of the variable
+// A variable name must start with a letter or the underscore character
+
+// A variable name cannot start with a number
+
+// A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+
+// Variable names are case-sensitive ($age and $AGE are two different variables)
+
+	// echo "Hello World";
+	// echo 'Hello';
+	
+	// echo "string";
+
+	// die();
+	// echo "<h1>Hello This is Kumar</h1>";
+
+	// echo $name_ = "Sharvan Kumar";
+	// echo "<br>";
+	// echo $name_;
+?>  
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo "Home Page"; ?></title>
+</head>
+<body>
+
+	<?php 
+		echo "<h1>".$firstName." ".$lastName."</h1>";
+	?>
+
+	<h1><?php echo $firstName . " ".$lastName; ?></h1>
+
+	<h1><?php echo "Kumar"; ?></h1>
+
+	<?php 
+		echo "<h2>Hello Sanjev</h2>";
+	?>
+
+	<a href="<?php echo "new.php" ?>">New Page</a>
+</body>
+</html>
---
+++ PHP/new.php
@@ -0,0 +1,183 @@
+<?php 
+	
+	$dayName =  date("D");
+
+	switch ($dayName) {
+		case 'Mon':
+			echo $dayName;
+			break;
+		case 'Tue':
+			echo $dayName;
+			break;
+		case 'Wed':
+			echo $dayName;
+			break;
+		case 'Thu':
+			echo $dayName;
+			break;
+		case 'Fri':
+			echo $dayName;
+			break;
+		case 'Sat':
+			echo $dayName;
+			break;
+		case 'Sun':
+			echo $dayName;
+			break;
+		
+		default:
+			echo "Error";
+			break;
+	}
+
+	die;
+
+	// if (condition) {
+	// 	# code...
+	// }
+
+	// $retVal = (condition) ? a : b ;
+
+	// $a = 200;
+
+	// $b = 45;
+
+	// echo ($a>$b) ? $a : $b;
+
+
+
+	// if (condition) {
+	// 	# code...
+	// } else {
+	// 	# code...
+	// }
+
+	// if (condition) {
+	// 	# code...
+	// } elseif {
+	// 	# code...
+	// }
+
+	// switch (variable) {
+	// 	case 'value':
+	// 		# code...
+	// 		break;
+		
+	// 	default:
+	// 		# code...
+	// 		break;
+	// }
+	
+	
+
+	if (isset($_POST['validate'])) {
+		
+		$fname = $_POST['fname'];
+
+		if ($fname=='') {
+			$className = 'is-invalid';
+		} else {
+			$className = "is-valid";
+		}
+	}
+
+	$fullName = "Kumar Amit";
+
+	echo (isset($fullName)) ? $fullName : '';
+
+	// echo $_GET['number'];	
+		
+	// if (isset($_GET['submit'])) {
+	// 	echo $_GET['number'];
+	// }
+
+	// if (isset($_GET['number'])) {
+	// 	echo $_GET['number'];
+	// }
+
+	//echo $_POST['number'];
+
+	// if (isset($_POST['number'])) {
+	// 	echo $_POST['number'];
+	// }
+
+	if (isset($_POST['submit'])) {
+		$number = $_POST['number'];
+
+		if ($number%2==0) {
+			$result =  $number." is Even";
+		} else {
+			$result =  $number. " is Odd";
+		}
+	}
+
+	if (isset($_POST['submit'])) {
+		$number = $_POST['number'];
+
+		if ($number%2==0) {
+			$result =  "<h3 style='color:blue'>".$number." is Even</h3>";
+		} else {
+			$result =  "<h3 style='color:red'>".$number." is Odd</h3>";
+		}
+	}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+</head>
+<body>
+
+	<form method="POST">
+		<label>Enter Your Number: </label>
+		<input type="text" name="number">
+		<input type="submit" name="submit">
+	</form>
+
+	<div class="container">
+		<form method="post">
+			<input type="text" name="fname" class="form-control <?php echo (isset($className)) ? $className : '' ?>">
+
+		<!-- 	<input type="text" name="fname" class="form-control <?php echo $className; ?>"> -->
+
+
+			<input type="submit" name="validate">
+		</form>
+	</div>
+
+	<?php 
+		if (isset($result)) {
+			?>
+
+			<h2 style="color: 
+			blue;"><?php echo $result; ?></h2>
+
+			<?php
+		}
+
+		if (isset($result)) {
+			echo "<h2 style='color:blue;'>".$result."</h2>";
+		}
+	?>
+
+	<?php
+		if (isset($result)) {
+			echo $result;
+		}
+	?>
+
+
+	<?php 
+
+		if (isset($fullName)) {
+			?>
+				<h5><?php echo $fullName; ?></h5>
+			<?php
+		}
+
+	?>
+
+</body>
+</html>
