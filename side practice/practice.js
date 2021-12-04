5599a962e76e85fa6bbe9e3e010ca0df0ad3bf41
russia99 <88884183+russia99@users.noreply.github.com>
2021-12-04 15:06:49 +0530

    done

--- side practice/practice.js
+++ side practice/practice.js
@@ -1,3 +1,19 @@
-let tabe = amit;
-let amit = `iam ${tabe}`;
-console.log(amit);
+let calcAverage = (value1, value2, value3) => 
+    average = (value1 + value2 + value3) / 3;
+    
+
+
+let totalAverage1 = calcAverage(44, 23, 71);
+let totalAverage2 = calcAverage(45, 25, 61);
+console.log(totalAverage1);
+console.log(totalAverage2);
+
+let checkWinner = (value1, value2) => {
+    if (value1 > value2) {
+        console.log("dolphin is winner");
+    } else {
+        console.log("you are winner");
+    }
+}
+
+checkWinner(totalAverage1, totalAverage2);
