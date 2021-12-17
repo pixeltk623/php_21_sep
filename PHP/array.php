f273a837c95d1ef0e72619ff1d24e1eda888122a
russia99 <88884183+russia99@users.noreply.github.com>
2021-12-17 11:22:33 +0530

    done

--- PHP/array.php
+++ PHP/array.php
@@ -1,13 +1,14 @@
-<?php 
+<?php
 
 // Indexed array 
 // associative Array 
 // Multidimensonal Array 
 
 
-function formatArray($arrayData) {
+function formatArray($arrayData)
+{
 	echo "<pre>";
-		print_r($arrayData);
+	print_r($arrayData);
 	echo "</pre>";
 }
 
@@ -17,7 +18,7 @@ function formatArray($arrayData) {
 
 //$list = array("Kumar",true, false, 45.656,69);
 
-//$list = array("Kumar","Sharvan","Test","Hello");
+// $list = array("amit" => "Kumar","Sharvan","Test","Hello");
 
 //formatArray($list);
 
@@ -34,7 +35,7 @@ function formatArray($arrayData) {
 // echo sizeof($list);
 
 // for ($i=0; $i < count($list); $i++) { 
-	
+
 // 	echo $list[$i];
 
 // 	echo "<br>";
@@ -42,7 +43,7 @@ function formatArray($arrayData) {
 
 
 // foreach ($list as $key => $value) {
-	
+
 // 	echo $key. " - " .$value;
 // 	echo "<br>";
 // }
@@ -93,14 +94,14 @@ function formatArray($arrayData) {
 
 
 // foreach ($list as $key => $value) {
-	
+
 // 	formatArray($value);
 // }
 
 //formatArray($list);
 
 // for ($i=0; $i < sizeof($list); $i++) { 
-	
+
 // 	formatArray($list[$i]);
 
 // }
@@ -159,11 +160,11 @@ $listOfStudents = array(
 // formatArray($listOfStudents);
 
 // for ($i=0; $i < sizeof($listOfStudents); $i++) { 
-	
+
 // 	echo $listOfStudents[$i]['name'];
 
 // }
-	
+
 
 
 
@@ -175,40 +176,42 @@ $listOfStudents = array(
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    
-    <div class="container mt-3">
-    	<h1 class="text-center text-primary">Array In PHP</h1>
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
-    	<table class="table table-bordered">
-			  <thead>
-			    <tr>
-			      <th scope="col" rowspan="2">Sr.No</th>
-			      <th scope="col" rowspan="2">Name</th>
-			      <th scope="col" rowspan="2">Email</th>
-			      <th scope="col" rowspan="2">Mobile</th>
-			      <th scope="col" rowspan="2">Gender</th>
-			      <th scope="col" rowspan="2">Status</th>
-			      <th scope="col" colspan="2">Address</th>
-			    </tr>
-			    <tr>
-			    	<th>City</th>
-			    	<th>Pin</th>
-			    </tr>
-			  </thead>
-			  <tbody>
-			  	<?php
-			  	/*
+	<title>Hello, world!</title>
+</head>
+
+<body>
+
+	<div class="container mt-3">
+		<h1 class="text-center text-primary">Array In PHP</h1>
+
+		<table class="table table-bordered">
+			<thead>
+				<tr>
+					<th scope="col" rowspan="2">Sr.No</th>
+					<th scope="col" rowspan="2">Name</th>
+					<th scope="col" rowspan="2">Email</th>
+					<th scope="col" rowspan="2">Mobile</th>
+					<th scope="col" rowspan="2">Gender</th>
+					<th scope="col" rowspan="2">Status</th>
+					<th scope="col" colspan="2">Address</th>
+				</tr>
+				<tr>
+					<th>City</th>
+					<th>Pin</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				/*
 			    <?php
 
 			    	if (count($listOfStudents)<1) {
@@ -240,37 +243,38 @@ $listOfStudents = array(
 				*/
 
 				foreach ($listOfStudents as $key => $value) {
-					?>
+				?>
 
 					<tr>
-					      <td><?php echo ($key+1); ?></td>
-					      <td><?php echo $value['name']; ?></td>
-					      <td><?php echo $value['email']; ?></td>
-					      <td><?php echo $value['mobile']; ?></td>
-					      <td><?php echo $value['gender']; ?></td>
-					      <td><?php echo $value['status']; ?></td>
-					      <td><?php echo $value['address']['city']; ?></td>
-					      <td><?php echo $value['address']['pin']; ?></td>
-					    </tr>
+						<td><?php echo ($key + 1); ?></td>
+						<td><?php echo $value['name']; ?></td>
+						<td><?php echo $value['email']; ?></td>
+						<td><?php echo $value['mobile']; ?></td>
+						<td><?php echo $value['gender']; ?></td>
+						<td><?php echo $value['status']; ?></td>
+						<td><?php echo $value['address']['city']; ?></td>
+						<td><?php echo $value['address']['pin']; ?></td>
+					</tr>
 
-					<?php
+				<?php
 				}
-			    ?>
-			  </tbody>
-			</table>
-    </div>
+				?>
+			</tbody>
+		</table>
+	</div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
+	<!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+	<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
+	<!-- Option 2: Separate Popper and Bootstrap JS -->
+	<!--
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
     -->
-  </body>
+</body>
+
 </html>
