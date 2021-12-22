b41c7c4cb54c3d7b6a9c78aa6a2d227e35640d0a
shreetuition <info@shreetuitioncenter.com>
2021-12-22 16:17:31 +0530

    Done

--- crud/index.php
+++ crud/index.php
@@ -1,89 +1,51 @@
-<?php
-include_once 'config.php';
+<?php 
+    include_once 'config.php';
 
-$query = "SELECT * FROM employees";
+    $query = "SELECT * FROM employees"; 
 
-$result = mysqli_query($conn, $query);
-  
-//formatArray($result);
+    $result = mysqli_query($conn, $query);
 
-//echo $result->num_rows;
+    //formatArray($result);
 
-// while ($response = mysqli_fetch_object($result)) {
+    //echo $result->num_rows;
 
-//     echo "<pre>";
+    // while ($response = mysqli_fetch_object($result)) {
+        
+    //     echo "<pre>";
 
-//     print_r($response[1]);
+    //     print_r($response[1]);
 
-// }
+    // }
 
-// die;
+    // die;
 
 ?>
 <!doctype html>
 <html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-
-  <title>Hello, world!</title>
-</head>
-
-<body>
-  <div class="container mt-3">
-    <h1 class="text-center text-primary">Crud In CORE PHP</h1>
-    <a href="create.php" class="btn btn-primary mb-3">Add New Employee</a>
-    <table class="table table-bordered">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Name</th>
-          <th scope="col">Email</th>
-          <th scope="col">Created At</th>
-          <th scope="col">Updated At</th>
-          <th scope="col">Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-
-        if ($result->num_rows > 0) { 
-
-          $sl = 1;
-          while ($response = mysqli_fetch_object($result)) {
-        ?>
+    <title>Hello, world!</title>
+  </head>
+  <body>
+     <div class="container mt-3">
+        <h1 class="text-center text-primary">Crud In CORE PHP</h1>
+        <a href="create.php" class="btn btn-primary mb-3">Add New Employee</a>
+        <table class="table table-bordered">
+          <thead>
             <tr>
-              <td><?php echo $sl++; ?></td>
-              <td><?php echo $response->name; ?></td>
-              <td><?php echo $response->email; ?></td>
-              <td><?php echo $response->created_at; ?></td>
-              <td><?php echo $response->updated_at; ?></td>
-              <td>
-                <a href="" class="btn btn-info">Show</a>
-                <a href="" class="btn btn-warning">Edit</a>
-                <a href="delete.php?id=<?php echo $response->id; ?>" class="btn btn-danger">Delete</a>
-              </td>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Created At</th>
+              <th scope="col">Updated At</th>
+              <th scope="col">Action</th>
             </tr>
-          <?php
-          }
-        } else {
-          ?>
-          <tr>
-            <td colspan="5" class="text-center text-danger">No Record Found</td>
-          </tr>
-
-        <?php
-        }
-
-        ?>
-      </tbody>
-    </table>
-  </div>
           </thead>
           <tbody>
             <?php 
@@ -127,19 +89,11 @@ $result = mysqli_query($conn, $query);
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
 
-
-  <!-- Optional JavaScript; choose one of the two! -->
-
-  <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
-
-  <!-- Option 2: Separate Popper and Bootstrap JS -->
-  <!--
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
     -->
-</body>
-
+  </body>
 </html>
