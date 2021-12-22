2dbf918ca05ca7833e71ab0e0a6fcce5417d41e6
russia99 <88884183+russia99@users.noreply.github.com>
2021-12-22 09:30:45 +0530

    done

--- side practice/practice.php
+++ side practice/practice.php
@@ -1,5 +1,79 @@
-<?php
-$number = 9;
-$str = "Beijing";
-printf("There are %u million bicycles in %s.",$number,$str);
-?> 
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <?php
+
+    if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+
+        // Connecting to the Database
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "amit_database";
+
+        // Create a connection
+        $conn = mysqli_connect($servername, $username, $password, $database);
+    
+
+    if (!$conn) {
+        die("Sorry we failed to connect: " . mysqli_connect_error());
+    } else {
+        $sql = "INSERT INTO `amit_table` (`name`, `email`) VALUES ('$name', '$email')";
+        $result = mysqli_query($conn, $sql);
+
+        if ($result) {
+            echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Success!</strong> Your entry has been submitted successfully!
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>';
+        } else {
+            echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+        <strong>Error!</strong> We are facing some technical issue and your entry ws not submitted successfully! We regret the inconvinience caused!
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">×</span>
+        </button>
+      </div>';
+        }
+    }
+}
+
+
+    ?>
+    <div class="container mt-3">
+        <h1 class="text-center text-primary">Crud In CORE PHP</h1>
+        <a href="practice2.php" class="btn btn-info mb-3">Home</a>
+        <form method="POST">
+            <div class="form-group">
+                <label>Name</label>
+                <input type="text" name="name" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="text" name="email" class="form-control">
+            </div>
+            <input type="submit" name="submit" class="btn btn-primary">
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
