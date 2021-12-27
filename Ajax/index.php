9826c40dd4586828de9fc408427eaf6722b68acd
russia99 <88884183+russia99@users.noreply.github.com>
2021-12-27 11:47:08 +0530

    done

--- Ajax/index.php
+++ Ajax/index.php
@@ -16,66 +16,6 @@
 
 <body>
     <div class="container mt-0">
-<<<<<<< HEAD
-        <h1 class="text-center text-primary" style="font-size: 20px;">Crud In CORE PHP</h1>
-        <a href="index.php" class="btn btn-info mb-1">Home</a>
-
-        <form method="post" enctype="multipart/form-data" id="uploadForm">
-            <div class="form-group">
-                <label>Name</label>
-                <input type="text" name="name" id="name" class="form-control form-control-sm">
-
-            </div>
-            <div class="form-group">
-                <label>Email</label>
-                <input type="text" name="email" id="email" class="form-control form-control-sm">
-
-            </div>
-
-            <div class="form-group">
-                <label>Gender</label>
-                <div class="form-check">
-                    <input type="radio" name="gender" value="Male" class='form-check-input'> Male
-                </div>
-                <div class="form-check">
-                    <input type="radio" name="gender" value="Female" class='form-check-input'> Female
-                </div>
-
-            </div>
-
-            <div class="form-group">
-                <label>Hobby</label>
-                <div class="form-check">
-                    <input type="checkbox" name="hobby" value="Cricket" class='form-check-input '> Cricket
-                </div>
-                <div class="form-check">
-                    <input type="checkbox" name="hobby" value="Football" class='form-check-input '> Football
-                </div>
-                <div class="form-check">
-                    <input type="checkbox" name="hobby" value="Baseball" class='form-check-input '> Baseball
-                </div>
-            </div>
-            <div class="form-group">
-                <label>City</label>
-                <select name="city" id="city" class="form-control form-control-sm">
-                    <option value="">Select</option>
-                    <option value="Vadodara">Vadodara</option>
-                    <option value="Surat">Surat</option>
-                    <option value="Anand">Anand</option>
-                    <option value="Bharuch">Bharuch</option>
-                </select>
-
-            </div>
-
-            <div class="form-group">
-                <label>Profile Pic</label>
-                <input type="file" name="profile_pic" id="profile_pic" class="form-control-file form-control-sm">
-
-
-            </div>
-            <input type="submit" name="submit" class="btn btn-primary addUser">
-        </form>
-=======
         <h1 class="text-center text-primary" style="font-size: 40px;">Crud In CORE PHP</h1>
         <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
            Add New Employee
@@ -89,18 +29,13 @@
         <?php
             include 'modal.php';
         ?>
->>>>>>> 650d9fdca855ed12976f4cbaec423b8edb90ab2b
     </div>
 
     <script type="text/javascript">
         $(document).ready(function() {
-<<<<<<< HEAD
-            $("#uploadForm").on('submit', function(e) {
-=======
             getAllData();
             $("#uploadForm").on('submit', function(e){
 
->>>>>>> 650d9fdca855ed12976f4cbaec423b8edb90ab2b
 
                 event.preventDefault();
 
@@ -115,15 +50,6 @@
                 let city = $("#city").val();
                 let hobby = $("[name=hobby]");
 
-<<<<<<< HEAD
-                let profile_pic = $("#profile_pic")[0].files[0].name;
-                console.log(profile_pic);
-                // var form_data = [];
-                // var file_data = $('#profile_pic').prop('files')[0];
-
-                for (let i = 0; i < gender.length; i++) {
-                    if (gender[i].checked) {
-=======
                 let profile_pic = $("#profile_pic");
                 //console.log(profile_pic.prop('files').length)
 
@@ -137,7 +63,6 @@
                                  
                 for(let i=0;i<gender.length; i++) {
                     if(gender[i].checked) {
->>>>>>> 650d9fdca855ed12976f4cbaec423b8edb90ab2b
                         genderValue = gender[i].value;
                     }
                 }
@@ -148,30 +73,6 @@
                     }
                 }
 
-<<<<<<< HEAD
-                let formAllData = {
-                    name: username,
-                    email: email,
-                    gender: genderValue,
-                    hobby: hobbyValue,
-                    city: city
-                };
-                console.log(formAllData);
-                // formAllData.push({file : file_data});
-                // console.log(formAllData);
-
-                $.ajax({
-                    type: 'POST',
-                    url: 'http://localhost/php_21_sep/ajax/insert.php',
-                    data: new FormData(this),
-                    contentType: false,
-                    cache: false,
-                    processData: false,
-                    success: function(resp) {
-                        console.log(resp);
-                    }
-                });
-=======
                 // let formAllData = {name: username, email : email, gender : genderValue,hobby : hobbyValue, city: city};
                 // console.log(formAllData);
                 // formAllData.push({file : file_data});
@@ -204,7 +105,6 @@
                             console.log(e)
                         }
                     });
->>>>>>> 650d9fdca855ed12976f4cbaec423b8edb90ab2b
 
             });
 
